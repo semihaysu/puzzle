0359864900976c2bd57528ada8755afa5ba8fc1f
Arif Eren <ai0000rf@gmail.com>
2023-04-07 15:28:56 +0300

    anasayfa ve text düzenlemeleri

--- puzzle/Start.Designer.cs
+++ puzzle/Start.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.buttonGiris = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.skorTabloText = new System.Windows.Forms.RichTextBox();
+            this.oyuncuAdText = new System.Windows.Forms.TextBox();
+            this.oyuncuAdLabel = new System.Windows.Forms.Label();
+            this.skorTabloLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonGiris
@@ -43,14 +46,46 @@
             this.buttonGiris.UseVisualStyleBackColor = true;
             this.buttonGiris.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
+            // skorTabloText
             // 
-            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.richTextBox1.Location = new System.Drawing.Point(146, 30);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(641, 252);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = " ";
+            this.skorTabloText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.skorTabloText.Location = new System.Drawing.Point(594, 71);
+            this.skorTabloText.Name = "skorTabloText";
+            this.skorTabloText.Size = new System.Drawing.Size(324, 287);
+            this.skorTabloText.TabIndex = 1;
+            this.skorTabloText.Text = " ";
+            this.skorTabloText.TextChanged += new System.EventHandler(this.skorTabloText_TextChanged);
+            // 
+            // oyuncuAdText
+            // 
+            this.oyuncuAdText.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.oyuncuAdText.Location = new System.Drawing.Point(217, 29);
+            this.oyuncuAdText.Name = "oyuncuAdText";
+            this.oyuncuAdText.Size = new System.Drawing.Size(209, 40);
+            this.oyuncuAdText.TabIndex = 4;
+            this.oyuncuAdText.TextChanged += new System.EventHandler(this.oyuncuAdText_TextChanged);
+            // 
+            // oyuncuAdLabel
+            // 
+            this.oyuncuAdLabel.AutoSize = true;
+            this.oyuncuAdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.oyuncuAdLabel.Location = new System.Drawing.Point(23, 32);
+            this.oyuncuAdLabel.Name = "oyuncuAdLabel";
+            this.oyuncuAdLabel.Size = new System.Drawing.Size(176, 33);
+            this.oyuncuAdLabel.TabIndex = 5;
+            this.oyuncuAdLabel.Text = "Oyuncu Adı";
+            this.oyuncuAdLabel.Click += new System.EventHandler(this.oyuncuAdLabel_Click);
+            // 
+            // skorTabloLabel
+            // 
+            this.skorTabloLabel.AutoSize = true;
+            this.skorTabloLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.skorTabloLabel.Location = new System.Drawing.Point(658, 32);
+            this.skorTabloLabel.Name = "skorTabloLabel";
+            this.skorTabloLabel.Size = new System.Drawing.Size(199, 33);
+            this.skorTabloLabel.TabIndex = 6;
+            this.skorTabloLabel.Text = "Skor Tablosu";
+            this.skorTabloLabel.Click += new System.EventHandler(this.skorTabloLabel_Click);
             // 
             // Start
             // 
@@ -58,19 +93,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(930, 384);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.skorTabloLabel);
+            this.Controls.Add(this.oyuncuAdLabel);
+            this.Controls.Add(this.oyuncuAdText);
+            this.Controls.Add(this.skorTabloText);
             this.Controls.Add(this.buttonGiris);
             this.Name = "Start";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Start_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonGiris;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox skorTabloText;
+        private System.Windows.Forms.TextBox oyuncuAdText;
+        private System.Windows.Forms.Label oyuncuAdLabel;
+        private System.Windows.Forms.Label skorTabloLabel;
     }
 }
 
